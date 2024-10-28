2ddda6622c518400fecaf81e8a396e55b36d173a
tanvirrifat1 <rifatkhan5567790@gmail.com>
2024-10-28 12:37:20 +0600

    complete faq

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from '../app/modules/auth/auth.route';
 
 import { UserRoutes } from '../app/modules/user/user.route';
 import { PetProfileRoutes } from '../app/modules/petProfile/petProfile.route';
+import { FaqRoutes } from '../app/modules/faq/faq.route';
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ const apiRoutes = [
   { path: '/user', route: UserRoutes },
   { path: '/auth', route: AuthRoutes },
   { path: '/petProfile', route: PetProfileRoutes },
+  { path: '/faq', route: FaqRoutes },
 ];
 
 apiRoutes.forEach(route => router.use(route.path, route.route));
